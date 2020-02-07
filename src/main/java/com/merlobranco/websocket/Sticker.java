48cfc93097f2d2b6dceaf3e7c01820d08cc0b9b1
Brais Cidras <braisomary@gmail.com>
2020-02-07 15:27:41 +0200

    Creating the Sticker Class

---
+++ src/main/java/com/merlobranco/websocket/Sticker.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.merlobranco.websocket;
+
+/**
+ *
+ * @author brais
+ */
+public class Sticker {
+
+    private int x;
+    private int y;
+    private String image;
+
+    public Sticker() {
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
